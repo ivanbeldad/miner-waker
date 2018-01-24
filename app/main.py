492f9797c105b4d18ddaf3ad243847dbb529b240
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-01-24 17:11:08 +0100

    build: fix app engine configuration

--- app/main.py
+++ app/main.py
@@ -1,17 +1,37 @@
 import alive_detector
 import tplink
+import webapp2
 
 
-def check():
-    """Check and reset down miners."""
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Miner Waker - Server Working')
 
-    print 'Check started...'
 
-    rigs = alive_detector.miners_alive()
+class Check(webapp2.RequestHandler):
+    def get(self):
+        """Check and reset down miners."""
 
-    for rig in rigs:
-        if rig['alive'] is False:
-            print 'Rig ' + rig['name'] + ' is down. Starting reset...'
-            tplink.reset(rig['name'])
+        print 'Check started...'
 
-    print 'Check finished'
+        rigs = alive_detector.miners_alive()
+
+        for rig in rigs:
+            if rig['alive'] is False:
+                print 'Rig ' + rig['name'] + ' is down. Starting reset...'
+                tplink.reset(rig['name'])
+
+        print 'Check finished'
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Done')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=False)
+
+check = webapp2.WSGIApplication([
+    ('/check', Check)
+], debug=False)
